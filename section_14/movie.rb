700f26a18793b25b96cfe0b9a21ef5054751f0d4
Bill LeBorgne <bill@leborgne.name>
2015-03-05 16:51:37 -0700

    [#89331264] Transfer files from section 13
    Copied files from section_13 to section_14 to work along
    with the section 14 video

---
+++ section_14/movie.rb
@@ -0,0 +1,33 @@
+class Movie
+  attr_reader :rank
+  attr_accessor :title
+
+  def initialize(title, rank = 0)
+    @title = title.capitalize
+    @rank = rank
+  end
+
+  def to_s
+    "#{@title} has a rank of #{@rank} (#{status})"
+  end
+
+  def normalized_rank
+    @rank / 10
+  end
+
+  def thumbs_up
+    @rank += 1
+  end
+
+  def thumbs_down
+    @rank -= 1
+  end
+
+  def hit?
+    @rank >= 10
+  end
+
+  def status
+    hit? ? 'Hit' : 'Flop'
+  end
+end
