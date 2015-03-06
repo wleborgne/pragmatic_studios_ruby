4bfd34d3e9cea9f2a075623b2acf41655fe18dbd
Bill LeBorgne <bill@leborgne.name>
2015-03-06 13:20:21 -0700

    [#89331264] Implement tests for WaldorfAndStatler
    Found solutions to my problem of not being able to directly
    test WaldorfAndStatler.review functionality.  As it can't be directly
    called, I had to implement a dummy class around the module call.
    
    These do seem to duplicate similar tests in playlist_spec, but since the
    Playlist.play method potentially does more than call this module, it
    could be argued that they are not strict duplicates.

--- section_14/spec/lib/waldorf_and_statler_spec.rb
+++ section_14/spec/lib/waldorf_and_statler_spec.rb
@@ -1,13 +1,35 @@
 require 'rspec'
 require_relative '../../lib/waldorf_and_statler'
+require_relative '../../lib/movie'
 
 describe WaldorfAndStatler do
-  let(:dummy_class) { Class.new { include(WaldorfAndStatler) } }
+  let(:dummy_class) do
+    Class.new do
+      include(WaldorfAndStatler)
+      def review(movie)
+        WaldorfAndStatler.review(movie)
+      end
+    end
+  end
 
-  # it '#review should return \'Bravo!\'' do
-  #   expect(dummy_class.new.review).to eq('Bravo!')
-  # end
-  # it 'should exist' do
-  #   expect(@dummy_class).to exist
-  # end
+  let(:initial_rank) { 10 }
+  let(:movie) { Movie.new('goonies', initial_rank) }
+
+  it 'gives the movie a thumbs up if a high number is rolled' do
+    allow(WaldorfAndStatler).to receive(:roll_die) { 5 }
+    dummy_class.new.review(movie)
+    expect(movie.rank).to eq(initial_rank + 1)
+  end
+
+  it 'skips a movie if a medium number is rolled' do
+    allow(WaldorfAndStatler).to receive(:roll_die) { 3 }
+    dummy_class.new.review(movie)
+    expect(movie.rank).to eq(initial_rank)
+  end
+
+  it 'gives the movie a thumbs down if a low number is rolled' do
+    allow(WaldorfAndStatler).to receive(:roll_die) { 1 }
+    dummy_class.new.review(movie)
+    expect(movie.rank).to eq(initial_rank - 1)
+  end
 end
