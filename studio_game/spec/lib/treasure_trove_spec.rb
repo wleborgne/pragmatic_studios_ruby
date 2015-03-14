22cf50e22be077339a45492a1abcddb155c0a316
Bill LeBorgne <bill@leborgne.name>
2015-03-14 17:09:34 -0600

    [#90256874] Add TreasureTrove module and spec

---
+++ studio_game/spec/lib/treasure_trove_spec.rb
@@ -0,0 +1,50 @@
+require 'rspec'
+require_relative '../../lib/treasure_trove'
+
+describe Treasure do
+  let(:treasure) { Treasure.new(:hammer, 50) }
+
+  it 'should be a Treasure' do
+    expect(treasure).to be_a(Treasure)
+  end
+
+  it 'has a name attribute' do
+    expect(treasure.name).to eq(:hammer)
+  end
+
+  it 'has a points attribute' do
+    expect(treasure.points).to eq(50)
+  end
+end
+
+describe TreasureTrove do
+  let(:trove) { TreasureTrove::TREASURES }
+
+  it 'has six treasures' do
+    expect(trove.size).to eq(6)
+  end
+
+  it 'has a pie worth 5 points' do
+    expect(trove[0]).to eq(Treasure.new(:pie, 5))
+  end
+
+  it 'has a bottle worth 25 points' do
+    expect(trove[1]).to eq(Treasure.new(:bottle, 25))
+  end
+
+  it 'has a hammer worth 50 points' do
+    expect(trove[2]).to eq(Treasure.new(:hammer, 50))
+  end
+
+  it 'has a skillet worth 100 points' do
+    expect(trove[3]).to eq(Treasure.new(:skillet, 100))
+  end
+
+  it 'has a broomstick worth 200 points' do
+    expect(trove[4]).to eq(Treasure.new(:broomstick, 200))
+  end
+
+  it 'has a crowbar worth 400 points' do
+    expect(trove[5]).to eq(Treasure.new(:crowbar, 400))
+  end
+end
